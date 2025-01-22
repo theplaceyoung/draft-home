72c8060fe3ad163211e2f2c9223960e322158c9e
Jang So Young <theplaceyoung@gmail.com>
2025-01-23 01:02:31 +0900

    add GA script

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart
@@ -0,0 +1,44 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get draft => 'DRAFT Company';
+
+  @override
+  String get exoticOrdinary => 'Exotic Ordinary';
+
+  @override
+  String get theExoticBoutique => 'The Exotic Boutique';
+
+  @override
+  String get dustyDraft => 'Dusty Draft';
+
+  @override
+  String get ordinaryLife => 'Ordinary Life';
+
+  @override
+  String get appTitle => 'DRAFT';
+
+  @override
+  String get homeTitle => 'Hello, Draft World!';
+
+  @override
+  String get homePage => 'Home';
+
+  @override
+  String get exoticOrdinaryPage => 'Exotic Ordinary';
+
+  @override
+  String get exoticBoutiquePage => 'The Exotic Boutique';
+
+  @override
+  String get dustyDraftPage => 'Dusty Draft';
+
+  @override
+  String get contactPage => 'Contact';
+}
---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_ko.dart
@@ -0,0 +1,44 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Korean (`ko`).
+class AppLocalizationsKo extends AppLocalizations {
+  AppLocalizationsKo([String locale = 'ko']) : super(locale);
+
+  @override
+  String get draft => '드래프트(draft)';
+
+  @override
+  String get exoticOrdinary => '이그조틱 오디너리';
+
+  @override
+  String get theExoticBoutique => '디 이그조틱 부띠끄';
+
+  @override
+  String get dustyDraft => '더스티 드래프트';
+
+  @override
+  String get ordinaryLife => '오디너리 라이프, 만세!';
+
+  @override
+  String get appTitle => '드래프트';
+
+  @override
+  String get homeTitle => '드래프트와 함께합시다!';
+
+  @override
+  String get homePage => '홈';
+
+  @override
+  String get exoticOrdinaryPage => '이그조틱오디너리';
+
+  @override
+  String get exoticBoutiquePage => '디 이그조틱 부띠끄';
+
+  @override
+  String get dustyDraftPage => '먼지낀 초안을 꺼내세요, 더스티드래프트.';
+
+  @override
+  String get contactPage => '근황공유 및 만나보기';
+}
