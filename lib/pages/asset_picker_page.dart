e4af2cc884fc3f1ceb2d4c887e9aed7b6c4d51b3
Jang So Young <soyoung@dustydraft.com>
2026-06-03 08:30:48 +0900

    wip: before branch switch

---
+++ lib/pages/asset_picker_page.dart
@@ -0,0 +1,20 @@
+import 'package:draft_home/widgets/app_bar.dart';
+import 'package:flutter/material.dart';
+
+class AssetPickerPage extends StatelessWidget {
+  const AssetPickerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CommonAppBar(
+        pageKey: 'assetpicker',
+      ),
+      body: const Center(
+        child: Text('ASSETPICKER'),
+
+        
+      ),
+    );
+  }
+}
